969cecd19faf386f4e65ce5188e00e5b3ec8e108
G Roques <groques360@gmail.com>
2018-04-07 16:35:13 -0500

    Add docstring

--- decision_tree/helpers.py
+++ decision_tree/helpers.py
@@ -2,4 +2,13 @@ from typing import List, Set
 
 
 def get_unique_values(rows: List, column_index: int) -> Set:
+    """Find the unique values for a column in a dataset.
+
+    Args:
+        rows: The rows of the dataset.
+        column_index: Column index to get the unique values of.
+
+    Returns:
+        A set of the unique values for the given column index.
+    """
     return set([row[column_index] for row in rows])
