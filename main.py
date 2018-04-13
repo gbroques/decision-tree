49f8a836a5ebfc10c6c115f74583292560ddac12
G Roques <groques360@gmail.com>
2018-04-12 19:25:49 -0500

    Read in data from a text file

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
+from argparse import ArgumentParser
+from typing import List
+
 from decision_tree import DecisionTree
 
 
 def main():
-    training_data = [
-        [1, 1, 1, 0],
-        [1, 1, 0, 1],
-        [0, 0, 1, 1],
-        [1, 1, 0, 0],
-        [1, 0, 0, 1]
-    ]
-    design_matrix = [row[:-1] for row in training_data]
-    target_values = [row[-1] for row in training_data]
+    data = read_data()
+    design_matrix = [row[:-1] for row in data]
+    target_values = [row[-1] for row in data]
     decision_tree = DecisionTree()
     decision_tree.fit(design_matrix, target_values)
     predictions = decision_tree.predict(design_matrix)
     decision_tree.print()
+
+
+def read_data() -> List[List]:
+    """Read in data from a text file."""
+    filename = get_filename()
+    with open(filename) as f:
+        f.readline()  # Skip first line
+        data = [line.split() for line in f]
+    return data
+
+
+def get_filename() -> str:
+    """Get the filename to read from as the first command line argument."""
+    parser = ArgumentParser(description='Build a decision tree from data stored in a text file.')
+    parser.add_argument('filename',
+                        metavar='filename',
+                        type=str,
+                        help='Data in the format described in specification.pdf')
+    args = parser.parse_args()
+    filename = vars(args)['filename']
+    return filename
 
 
 if __name__ == '__main__':
