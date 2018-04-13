606cb5cb7e4d140704e5c07c5b908f24644ed736
G Roques <groques360@gmail.com>
2018-04-12 19:50:01 -0500

    Enter a loop and prompt user to enter a test example

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def main():
     target_values = [row[-1] for row in data]
     decision_tree = DecisionTree()
     decision_tree.fit(design_matrix, target_values)
-    predictions = decision_tree.predict(design_matrix)
     decision_tree.print()
+    num_features = len(data[0]) - 1
+    wait_for_test_example(decision_tree, num_features)
 
 
 def read_data() -> List[List]:
@@ -33,6 +34,19 @@ def get_filename() -> str:
     args = parser.parse_args()
     filename = vars(args)['filename']
     return filename
+
+
+def wait_for_test_example(decision_tree: DecisionTree, num_features: int):
+    while True:
+        try:
+            example = input('Please enter a test example delimited by whitespace: ')
+            example = example.split()
+            if len(example) != num_features:
+                raise ValueError()
+            prediction = decision_tree.predict_example(example)
+            print('The tree predicts ' + str(prediction))
+        except ValueError:
+            pass
 
 
 if __name__ == '__main__':
